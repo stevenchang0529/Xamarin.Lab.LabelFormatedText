dafb0a8ea644343b983acca7305b71400d0e0a41
Steven <steven@MSI-Steven>
2019-12-16 17:09:53 +0800

    加入專案檔案。

---
+++ Xamarin.Lab.LabelFormatedText/Xamarin.Lab.LabelFormatedText/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Lab.LabelFormatedText
+{
+    public class MainViewModel
+    {
+        public int TeamA { get; set; } = 20;
+        public int TeamB { get; set; } = 10;
+    }
+}
